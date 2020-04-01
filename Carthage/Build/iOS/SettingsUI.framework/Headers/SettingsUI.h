3727ee803e38a85691693190fcac84136d6ecca4
Pablo Villar <pablolvillar@gmail.com>
2020-04-01 10:29:32 -0300

    Add carthage support

---
+++ Carthage/Build/iOS/SettingsUI.framework/Headers/SettingsUI.h
@@ -0,0 +1,19 @@
+//
+//  SettingsUI.h
+//  SettingsUI
+//
+//  Created by Pablo Villar on Mar 22, 2020.
+//  Copyright © 2020 volbap. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SettingsUI.
+FOUNDATION_EXPORT double SettingsUIVersionNumber;
+
+//! Project version string for SettingsUI.
+FOUNDATION_EXPORT const unsigned char SettingsUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SettingsUI/PublicHeader.h>
+
+
